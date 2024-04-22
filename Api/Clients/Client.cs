68a8387354a95f37377a796a90d143869e6a4703
Agamenon Junior <agamenon_422@hotmail.com>
2024-04-21 21:24:02 -0300

    criando a class Client e limpando parte do program.cs

---
+++ Api/Clients/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Clients;
+
+public class Client
+{
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Cellphone { get; set; }
+}
