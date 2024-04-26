726372b0756533d12696a1911e4de4a7f861ce40
Agamenon Junior <agamenon_422@hotmail.com>
2024-04-26 19:18:01 -0300

    Criada a migration initial

--- Api/Client/Client.cs
+++ Api/Client/Client.cs
@@ -4,6 +4,8 @@ public class Client
 {
     public int Id { get; init; }
     public string Name { get; set; }
+    public string Email { get; set; }
     public string Address { get; set; }
     public string Cellphone { get; set; }
+
 }
--- Api/Client/ClientRoutes.cs
+++ Api/Client/ClientRoutes.cs
@@ -13,7 +13,7 @@ namespace Api.Client
             app.MapPost("clients", (ExecuteSqlRequest request, AppDbContext context) =>
             {
 
-                var result = context.Database.SqlQueryRaw<Client>(request.SqlCode).ToList();
+                var result = context.Database.SqlQueryRaw<Client>($"{request.SqlCode}").ToList();
 
                 return result;
 
