faec41920bbc00ddc8fdc0b328b3b99af27c9973
Agamenon Junior <agamenon_422@hotmail.com>
2024-04-26 21:11:07 -0300

    criada a logica de execucao da requisicao da API

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+    const [sqlInput, setSqlInput] = useState("");
+    const [jsonInput, setJsonInput] = useState("");
+
+    const handleButton=()=>{
+      //Obtem o valor da constante SQL de entrada;
+      alert(sqlInput);
+
+      
+
+      //Envia o HTTP POST p/ a API;
+
+
+
+      //Atribue o resultado no campo JSON;
+      setJsonInput("Teste");
+    }
+
   return (
    <div className='bg-bottom bg-no-repeat bg-slate-50 dark:bg-[#0B1120] h-screen p-10'>
       <div>
@@ -10,19 +29,30 @@ function App() {
       <div className='grid grid-cols-2 gap-4 py-10'>
         <div>
           <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Comando SQL:</label>
-          <input className='w-full h-32 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"'></input>
+          <input 
+            onChange={(e)=>setSqlInput(e.target.value)}
+            className='w-full h-32 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"'></input>
         </div>
 
         <div>
           <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Saída JSON:</label>
-          <input className='w-full h-32 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"'></input>
+          <input
+            value={jsonInput}
+            className='w-full h-32 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"'>
+
+            </input>
         </div>
         
         
       </div>
 
       <div className='text-center'>
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Executar comando</button>
+        <button 
+          onClick={(e)=>handleButton()}
+          type="submit" 
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Executar comando
+          </button>
 
       </div>
    </div>
